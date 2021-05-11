d42511483ffbfa11b4a98124af875ecaac722516
aBuBili <70617237+aBuBili@users.noreply.github.com>
2021-05-11 10:54:52 +0800

    Fix： lef->leftRef; css left->translateX()
    Fix： lef->leftRef; css left->translateX()

--- src/components/slideTab.vue
+++ src/components/slideTab.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container">
-    <div class="tabbar" :style="{ '--left': lef, '--width': wid }">
+    <div
+      class="tabbar"
+      :style="{ '--leftRef': leftRef, '--widthRef': widthRef }"
+    >
       <p
         class="item"
-        v-for="(e, k) of listArr"
+        v-for="(e, k) of navList"
         :key="k"
         @click="onHandleClick($event, k)"
         :class="{ active: activeIndex == k }"
@@ -16,21 +19,25 @@
 
 <script setup>
 import { defineProps, reactive, ref } from "vue";
-const { navList } = defineProps();
-console.log(navList);
+// props
+defineProps({
+  navList: Array,
+});
 
-const listArr = ["Vue", "React", "Home"];
+//当前选中item
 const activeIndex = ref(0);
-const lef = ref(0);
-const wid = ref("70px");
+
+// 根据评论区 “_月无情” 进行更改 lef 更改为leftRef 可以 见名知意 时刻知道它是一个ref
+const leftRef = ref(0);
+const widthRef = ref("70px");
 
 function onHandleClick({ target }, index) {
-  const { width, left } = target.getBoundingClientRect();
-  const { left: parentLeft } = target.parentNode.getBoundingClientRect();
+  const { width, left } = target.getBoundingClientRect(); //当前元素获取width 和 left
+  const { left: parentleftRef } = target.parentNode.getBoundingClientRect(); //父级元素获取left
 
-  lef.value = left - 4 - parentLeft + "px";
-  wid.value = width + "px";
-  activeIndex.value = index;
+  leftRef.value = left - 4 - parentleftRef + "px"; //计算滑块应该在的距离
+  widthRef.value = width + "px"; //计算滑块宽度
+  activeIndex.value = index; //设置 active item
 }
 </script>
 
@@ -58,21 +65,25 @@ function onHandleClick({ target }, index) {
   padding: 10px 20px;
   z-index: 10;
   cursor: pointer;
+  /* 设置item 文字变化效果 以免闪 */
+  transition: 0.1s;
 }
 
 .active {
   color: #fff;
 }
 
+/* 伪元素 滑块 */
 .tabbar::before {
   position: absolute;
   content: "";
-  width: var(--width);
+  width: var(--widthRef);
   height: 100%;
   background-color: #598bf0;
   border-radius: 24px;
-  left: var(--left);
-  transition: left 0.4s;
+  /* 根据评论区 “_月无情” 进行更改 lef 更改为translateX 更高效 */
+  transform: translateX(var(--leftRef));
+  transition: 0.2s;
   transition-timing-function: ease-in-out;
 }
 </style>
