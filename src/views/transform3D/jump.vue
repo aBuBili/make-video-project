2fdc22383b2bf9039671b3262b689b858cbb6e5e
aBuBili <70617237+aBuBili@users.noreply.github.com>
2021-05-18 14:32:50 +0800

    面包屑导航
    面包屑导航

--- src/views/transform3D/jump.vue
+++ src/views/transform3D/jump.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="container">
-    <div class="content">
-      <div class="front"></div>
-      <div class="back"></div>
-      <div class="left"></div>
-      <div class="right"></div>
-      <div class="top"></div>
-      <div class="bottom"></div>
-    </div>
+    <template v-for="e of boxList" :key="e">
+      <div
+        class="content"
+        :style="{
+          top: e.x,
+          left: e.y,
+        }"
+      >
+        <div
+          class="left"
+          :style="{
+            'background-image': 'url(' + e.left + ')',
+          }"
+        ></div>
+        <div
+          class="right"
+          :style="{
+            'background-image': 'url(' + e.left + ')',
+          }"
+        ></div>
+        <div
+          class="top"
+          :style="{
+            'background-image': 'url(' + e.left + ')',
+          }"
+        ></div>
+      </div>
+    </template>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const boxList = [
+  {
+    x: "300px",
+    y: "180px",
+    top: "../../../src/assets/transform3D/green-top.png",
+    left: "../../../src/assets/transform3D/green-top.png",
+    right: "../../../src/assets/transform3D/green-top.png",
+  },
+  {
+    x: "300px",
+    y: "400px",
+    top: "../../../src/assets/transform3D/green-top.png",
+    left: "../../../src/assets/transform3D/green-top.png",
+    right: "../../../src/assets/transform3D/green-top.png",
+  },
+];
+</script>
 
 <style scoped>
 .container {
@@ -19,27 +56,25 @@
   width: 600px;
   min-height: 100%;
 }
+
 .content {
-  /* perspective: 300px; */
   position: fixed;
-  top: 300px;
-  left: 80px;
+  /* top: var(--x);
+  left: var(--y); */
   position: relative;
   transform-style: preserve-3d;
-  transform: rotateX(-30deg) rotateY(-45deg);
+  transform: rotateX(-35deg) rotateY(-45deg);
   transition: all 1s;
-  /* animation: run 10s infinite; */
 }
 
 .content div {
   position: absolute;
 }
 
-.front {
+.left {
   width: 100px;
   height: 70px;
   transform: translateZ(50px);
-  background-image: url("../../assets/transform3D/orangeLeft.png");
   background-size: cover;
   filter: contrast(60%);
   -webkit-filter: contrast(60%); /* Chrome, Safari, Opera */
@@ -49,7 +84,7 @@
   height: 70px;
   left: 50px;
   transform: rotateY(90deg);
-  background-image: url("../../assets/transform3D/orangeLeft.png");
+  background-image: url("../../../src/assets/transform3D/green-top.png");
   background-size: cover;
   filter: contrast(88%);
   -webkit-filter: contrast(88%); /* Chrome, Safari, Opera */
@@ -59,44 +94,7 @@
   height: 100px;
   top: -50px;
   transform: rotateX(90deg);
-  background-image: url("../../assets/transform3D/orangeTop.png");
+  background-image: url("../../../src/assets/transform3D/green-top.png");
   background-size: cover;
-}
-
-/* .back {
-  width: 100px;
-  height: 70px;
-  transform: translateZ(-60px) rotateY(180deg);
-  background-color: rgba(36, 255, 1, 0.5);
-}
-.left {
-  width: 100px;
-  height: 70px;
-  left: -60px;
-  transform: rotateY(-90deg);
-  background-color: rgba(25, 62, 75, 0.5);
-}
-
-.bottom {
-  width: 100px;
-  height: 120px;
-  top: 10px;
-  transform: rotateX(-90deg);
-  background-color: rgba(255, 62, 26, 0.5);
-} */
-@keyframes run {
-  0% {
-    transform: rotateY(0);
-  }
-  50% {
-    transform: rotateY(180deg);
-  }
-  100% {
-    transform: rotateY(360deg);
-  }
-  /* 20% {
-    transform: rotateX(180deg);
-    transform: rotateY(180deg);
-  } */
 }
 </style>
---
+++ src/views/transform3D/round.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="front"></div>
+      <div class="back"></div>
+      <div class="left"></div>
+      <div class="right"></div>
+      <div class="top"></div>
+      <div class="bottom"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.container {
+  background-image: linear-gradient(#d8dae5, #bcbec6);
+  width: 600px;
+  min-height: 100%;
+}
+.content {
+  /* perspective: 300px; */
+  position: fixed;
+  top: 300px;
+  left: 80px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateX(-35deg) rotateY(-45deg);
+  transition: all 1s;
+  /* animation: run 10s infinite; */
+}
+
+.content div {
+  position: absolute;
+}
+
+.left {
+  width: 100px;
+  height: 70px;
+  transform: translateZ(50px);
+  background-image: url("../../assets/transform3D/green-top.png");
+  background-size: cover;
+  filter: contrast(60%);
+  -webkit-filter: contrast(60%); /* Chrome, Safari, Opera */
+}
+.right {
+  width: 100px;
+  height: 70px;
+  left: 50px;
+  transform: rotateY(90deg);
+  background-image: url("../../assets/transform3D/green-top.png");
+  background-size: cover;
+  filter: contrast(88%);
+  -webkit-filter: contrast(88%); /* Chrome, Safari, Opera */
+}
+.top {
+  width: 100px;
+  height: 100px;
+  top: -50px;
+  transform: rotateX(90deg);
+  background-image: url("../../assets/transform3D/green-top.png");
+  background-size: cover;
+}
+
+/* .back {
+  width: 100px;
+  height: 70px;
+  transform: translateZ(-60px) rotateY(180deg);
+  background-color: rgba(36, 255, 1, 0.5);
+}
+.left {
+  width: 100px;
+  height: 70px;
+  left: -60px;
+  transform: rotateY(-90deg);
+  background-color: rgba(25, 62, 75, 0.5);
+}
+
+.bottom {
+  width: 100px;
+  height: 120px;
+  top: 10px;
+  transform: rotateX(-90deg);
+  background-color: rgba(255, 62, 26, 0.5);
+} */
+@keyframes run {
+  0% {
+    transform: rotateY(0);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+  /* 20% {
+    transform: rotateX(180deg);
+    transform: rotateY(180deg);
+  } */
+}
+</style>
